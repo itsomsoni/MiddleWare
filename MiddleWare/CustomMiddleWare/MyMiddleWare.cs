1efefdb9a62681107639d6034784ac7d8d76cea6
Om Soni <soniom2002@gmail.com>
2024-07-21 11:36:28 +0530

    Custom Middleware Practice

---
+++ MiddleWare/CustomMiddleWare/MyMiddleWare.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MiddleWare.CustomMiddleWare
+{
+    public class MyMiddleWare : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My Custom MiddleWare Has Runned.\n");
+            await next(context);
+        }
+    }
+}
--- MiddleWare/Program.cs
+++ MiddleWare/Program.cs
@@ -1,6 +1,24 @@
+using MiddleWare.CustomMiddleWare;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyMiddleWare>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Welcome To ASP.NET Core App. Also MiddleWare First.\n");
+    await next(context);
+});
+//custom middleware
+app.UseMiddleware<MyMiddleWare>();
+
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("This Is" +
+        " MiddleWare - 3\n");
+});
 
 app.Run();
